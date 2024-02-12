3a27e850f7db6ba4f3bba876c9364d1b5f6a46d5
Bhautik Patel <thummarbhautik7045@gmail.com>
2024-02-12 10:46:49 +0530

    Variables, Dynamic, var, Const,final,primitive,nullabale

---
+++ mydart4.dart
@@ -0,0 +1,35 @@
+void main() {
+  String gt = "helloword";
+  gt = '\$gt. Yoo';
+  gt = 'hello\ndart';
+  print(gt);
+
+
+  var a = '10';
+  final b ="1";
+  const c = "26";
+  print(a);
+  print(b);
+  print(c);
+  print("---------");
+  a = 'hello';
+  // b = "88";
+  // c = "546";
+
+
+// final a = DateTime.now();
+// print(a);
+// const b  = DateTime.now();
+// print(b);
+
+// Optinal Varibale
+
+//  String? some = "54" ;
+//     print(some.length);
+
+// a = '10';
+print(a?.length??0);
+
+ }
+
+ String? a;
