3a42f639309efccc63029c0d5e3ccf7ed900c628
Bhautik Patel <thummarbhautik7045@gmail.com>
2024-02-16 18:31:06 +0530

    Extensions

---
+++ myDart28.dart
@@ -0,0 +1,16 @@
+void main() {
+String motivation = "this is Day";
+  motivation = motivation.capitilizeFirstChar();
+  print(motivation);
+  
+  String name = 'myName';
+  name = name.capitilizeFirstChar();
+  print(name);
+  
+}
+
+extension CapitalizefirstcharofString on String {
+  String capitilizeFirstChar(){
+    return this[0].toUpperCase() + this.substring(1);
+  }
+}
