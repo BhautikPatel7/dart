cf42684ed30b9727b29916c69da44652796bdbc3
Bhautik Patel <thummarbhautik7045@gmail.com>
2024-02-16 18:40:44 +0530

    Completed Dart Tutorials

--- myDart28.dart
+++ myDart28.dart
@@ -1,16 +1,15 @@
 void main() {
-String motivation = "this is Day";
+  String motivation = "this is Day";
   motivation = motivation.capitilizeFirstChar();
   print(motivation);
-  
+
   String name = 'myName';
   name = name.capitilizeFirstChar();
   print(name);
-  
 }
 
 extension CapitalizefirstcharofString on String {
-  String capitilizeFirstChar(){
+  String capitilizeFirstChar() {
     return this[0].toUpperCase() + this.substring(1);
   }
 }
